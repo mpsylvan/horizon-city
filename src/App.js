5d73dd11f2e9640f4d6c273b1babdf9443940db9
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-02-06 14:23:04 -0500

    testing remote repo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Horizon City meet and explore
         </p>
         <a
           className="App-link"
