d3a331fcda16bb471af4185c81bd00468806dac8
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-02-07 09:34:49 -0500

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Horizon City meet and explore!!
+          Horizon City meet
         </p>
         <a
           className="App-link"
