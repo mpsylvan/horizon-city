c8841ff030416b15cfab2531fa493ead4720757f
Michael <micperdo@gmail.com>
2023-02-09 13:24:00 -0500

    includes updated tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import App from '../App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App";
 import EventList from "../EventList";
 import CitySearch from "../CitySearch";
+import NumberOfEvents from "../NumberOfEvents";
 
 // test scope for App component
-describe('<App /> component', ()=>{
+describe("<App /> component", () => {
+  // consolidate the shallow wrapper into one beforeAll function.
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
 
-    // consolidate the shallow wrapper into one beforeAll function.
-    let AppWrapper;
-    beforeAll(()=>{
-        AppWrapper = shallow(<App />);
-    })
+  test("render single event list", () => {
+    // expect only 1 Event List component within AppWrapper
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+  test("<City Search> component", () => {
+    // expect 1 CitySearch component to exist
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
 
-    test('render single event list', ()=>{
-        // expect only 1 Event List component within AppWrapper 
-        expect(AppWrapper.find(EventList)).toHaveLength(1);
-    })
-    test('City Search', ()=>{
-        // expect 1 CitySearch component to exist
-        expect(AppWrapper.find(CitySearch)).toHaveLength(1);
-    })
+  test("<NumberOfEvents> component", () => {
+    // expect 1 NumberOfEvents component to exist
+    expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+  });
 });
-
