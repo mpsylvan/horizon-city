2782c034e9c50d974e1159603cb48956ee4287fa
Michael <micperdo@gmail.com>
2023-02-13 11:47:20 -0500

    uses react17 render methodn instead of createRoot react18 method

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const root = document.getElementById("root");
+render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
 
 // If you want your app to work offline and load faster, you can change
