08215115057ed51b4569e4ea19680277c5dbc834
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-02-14 16:31:58 -0500

    merge

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from "react";
-// import ReactDOM from "react-dom/client";
-import { render } from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>,
+  root
   </React.StrictMode>,
   root
 );
