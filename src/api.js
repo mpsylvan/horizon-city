fccf643eb2e2f5dd96fee890a2cc0b9bd8f7741e
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-02-14 16:30:55 -0500

    merge

--- src/api.js
+++ src/api.js
@@ -1,91 +1,12 @@
-// utility file for all api calls and processing functions
+// utility file for all api calls and processing functions 
 import { mockData } from "./mock_data";
-import axios from "axios";
 
-export const extractLocations = (events) => {
-  var extractLocations = events.map((event) => event.location);
-  const locations = [...new Set(extractLocations)];
-  return locations;
-};
+export const extractLocations = (events) =>{
+    var extractLocations = events.map((event)=> event.location);
+    const locations = [...new Set(extractLocations)];
+    return locations;
+}
 
-export const checkToken = async (accessToken) => {
-  const result = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  )
-    .then((res) => res.json())
-    .catch((err) => err.json());
-
-  return result;
-};
-
-export const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const { access_token } = await fetch(
-    `https://hts1nbzm17.execute-api.us-east-1.amazonaws.com/dev/api/token/${encodeCode}`
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .catch((err) => err);
-
-  access_token && localStorage.setItem("access_token", access_token);
-
-  return access_token;
-};
-
-export const removeQuery = () => {
-  if (window.history.pushState && window.location.pathname) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
-};
-
-export const getEvents = async () => {
-  if (window.location.href.startsWith("http://localhost")) {
+export const getEvents = async ()=>{
     return mockData;
-  }
-
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery();
-    const url = `https://hts1nbzm17.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${token}`;
-    const result = await axios.get(url);
-    if (result.data) {
-      var locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data));
-      localStorage.setItem("locations", JSON.stringify(locations));
-    }
-
-    return result.data.events;
-  }
-};
-
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem("access_token");
-
-  // return a valid access token or
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const results = await axios.get(
-        "https://hts1nbzm17.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const { authURL } = results.data;
-      return (window.location.href = authURL);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
-};
+}
