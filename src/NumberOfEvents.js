2c38957607fb10492f6a0791c549ae004275dd5a
Michael <micperdo@gmail.com>
2023-02-18 16:06:34 -0500

    updates spacing

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,40 +1,34 @@
 import React, { Component } from "react";
 
-class NumberOfEvents extends Component{
-    
-   state = {
-        number : 0
-    }
-    
+class NumberOfEvents extends Component {
+  state = {
+    number: 0,
+  };
 
-    handleInputChange = (event) =>{
-        const value = event.target.value;
-        this.setState({
-            number: value,
-        });
-        this.props.updateEvents(undefined, value);
-    };
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      number: value,
+    });
+    this.props.updateEvents(undefined, value);
+  };
 
-  
-
-    render(){
-        
-        return(
-            <>
-                <label>
-                    Select how many events to show: 
-                    <input 
-                        className='numberInput'
-                        type = "number"
-                        value = {this.state.number}
-                        onChange={this.handleInputChange}
-                        min = "1"
-                    />
-                </label>
-            </>
-            
-        )
-    }
+  render() {
+    return (
+      <>
+        <label>
+          Select how many events to show:
+          <input
+            className="numberInput"
+            type="number"
+            value={this.state.number}
+            onChange={this.handleInputChange}
+            min="1"
+          />
+        </label>
+      </>
+    );
+  }
 }
 
 export default NumberOfEvents;
