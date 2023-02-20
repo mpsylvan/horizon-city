c82a4c50cc8b357ac34e2b12afd3b0e054bd83e1
Michael <micperdo@gmail.com>
2023-02-20 17:26:15 -0500

    includes error alert class component and rendering condition

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
     number: 0,
+    errorText: "",
   };
 
   handleInputChange = (event) => {
     const value = event.target.value;
-    this.setState({
-      number: value,
-    });
+    if (value > 32 || value < 0)
+      this.setState({
+        number: value,
+        errorText: "only values from 1 - 32 can be specified.",
+      });
+    else {
+      this.setState({
+        number: value,
+        errorText: "",
+      });
+    }
     this.props.updateEvents(undefined, value);
   };
 
   render() {
     return (
-      <>
+      <div className="NOE" style={{ display: "flex", flexDirection: "column" }}>
         <label>
           Select how many events to show:
           <input
@@ -26,7 +36,8 @@ class NumberOfEvents extends Component {
             min="1"
           />
         </label>
-      </>
+        <ErrorAlert text={this.state.errorText} />
+      </div>
     );
   }
 }
