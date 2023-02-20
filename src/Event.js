a84e404c006fc0bf176eb9e511aeaea7052ebd4d
Michael <micperdo@gmail.com>
2023-02-20 17:26:27 -0500

    slight style tweaks

--- src/Event.js
+++ src/Event.js
@@ -1,7 +1,4 @@
-import React, {Component} from "react";
-
-
-
+import React, { Component } from "react";
 
 /* 
 
@@ -14,46 +11,54 @@ class Event extends Component {
     details: false,
   };
 
-    handleShowDetails = (event)=>{
-        this.setState({
-            details: true,
-        })
-    }
-    
-    handleHideDetails = (event)=>{
-       this.setState({
-        details: false,
-       })
-    }
+  handleShowDetails = (event) => {
+    this.setState({
+      details: true,
+    });
+  };
 
-    render(){
-        const {event} = this.props;
-        return (
-            <div className="eventCard"style={{border: "1px solid black", margin: "5px", background: "#7a8", color: "#fff"}}>
-                <h3 className="eventTitle">{event.summary}</h3>
-                <p className="eventTime">{event.start.dateTime}({event.start.timeZone})</p>
-                <p className="eventLocation" style={{color: "#000"}}>@{event.summary} || {event.location}</p>
-                
-                {!this.state.details?
-                    (
-                        <>
-                            <button className= "showDetails" onClick = {this.handleShowDetails}>Show Details</button>
-                        </>
-                    )
-                            :
-                    (
-                        <>
-                            <button className= "collapseDetails" onClick ={this.handleHideDetails}>Collapse Details</button>
-                            <h4 className="aboutHeader">About this event:</h4>
-                            <a className="calendarLink" href={event.htmlLink}>See details on Google Calendar</a>
-                            <p className="description">{event.description}</p>
-                        </>
-                    )
-                }
-                
-            </div>
-        )
-    }
+  handleHideDetails = (event) => {
+    this.setState({
+      details: false,
+    });
+  };
+
+  render() {
+    const { event } = this.props;
+    return (
+      <div className="eventCard" style={{ margin: "5px" }}>
+        <h3 className="eventTitle">{event.summary}</h3>
+        <p className="eventTime">
+          {event.start.dateTime}({event.start.timeZone})
+        </p>
+        <p className="eventLocation" style={{ color: "#000" }}>
+          @{event.summary} || {event.location}
+        </p>
+
+        {!this.state.details ? (
+          <>
+            <button className="showDetails" onClick={this.handleShowDetails}>
+              Show Details
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              className="collapseDetails"
+              onClick={this.handleHideDetails}
+            >
+              Collapse Details
+            </button>
+            <h4 className="aboutHeader">About this event:</h4>
+            <a className="calendarLink" href={event.htmlLink}>
+              See details on Google Calendar
+            </a>
+            <p className="description">{event.description}</p>
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Event;
