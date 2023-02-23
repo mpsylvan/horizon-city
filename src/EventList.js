9cc9e2af1dcb8192564cb3eac21b4a07f9a60fe1
Michael <micperdo@gmail.com>
2023-02-23 18:18:15 -0500

    alerts user to use of CacheData when working offline

--- src/EventList.js
+++ src/EventList.js
@@ -1,17 +1,44 @@
 import React, { Component } from "react";
 import Event from "./Event";
+import { CacheData } from "./Alert";
 
 class EventList extends Component {
+  state = {
+    showCacheAlert: true,
+  };
+
+  hideAlert = () => {
+    this.setState({
+      showCacheAlert: false,
+    });
+  };
+
   render() {
     const { events } = this.props;
     return (
-      <ul className="EventList">
-        {events.map((event) => (
-          <li key={event.id}>
-            <Event event={event} />
-          </li>
-        ))}
-      </ul>
+      <div>
+        {!navigator.onLine ? (
+          <div
+            onClick={this.hideAlert}
+            style={
+              this.state.showCacheAlert
+                ? { display: "flex", justifyContent: "center" }
+                : { display: "none" }
+            }
+          >
+            <CacheData text="Data is coming from cache, internet connection required for updated events, click/tap to hide " />
+          </div>
+        ) : (
+          <></>
+        )}
+        <ul className="EventList">
+          {events.map((event) => (
+            <li key={event.id}>
+              <Event event={event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
