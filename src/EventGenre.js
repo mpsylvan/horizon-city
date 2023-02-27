acfc52b5fe71a470dfa6000aad12e5373f92f27b
Michael <micperdo@gmail.com>
2023-02-27 13:26:36 -0500

    colors the pie chart based on summary content

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -4,13 +4,14 @@ import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
 
+  const colors = ["#ade", "#26b", "#2cc", "#58d", "#22f"];
+
   const getData = () => {
     const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
     const data = genres.map((genre) => {
       const value = events.filter((event) =>
         event.summary.split(" ").includes(genre)
       ).length;
-      console.log(genre);
       return { name: genre, value };
     });
     return data;
@@ -27,13 +28,17 @@ const EventGenre = ({ events }) => {
           cx={200}
           cy={200}
           labelLine={false}
-          outerRadius={80}
+          outerRadius={100}
           fill="#8884d8"
           dataKey="value" // Genre
           label={({ name, percent }) =>
             `${name} ${(percent * 100).toFixed(0)}%.`
           }
-        ></Pie>
+        >
+          {getData().map((entry, index) => (
+            <Cell key={`cell ${index}`} fill={colors[index]} />
+          ))}
+        </Pie>
       </PieChart>
     </ResponsiveContainer>
   );
